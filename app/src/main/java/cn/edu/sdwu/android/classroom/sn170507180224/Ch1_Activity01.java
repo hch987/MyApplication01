c455c25a8e400763ed6511b074783d18bbf2ceca
hch987 <1933199695@qq.com>
2020-04-09 15:09:29 +0800

    111

---
+++ app/src/main/java/cn/edu/sdwu/android/classroom/sn170507180224/Ch1_Activity01.java
@@ -0,0 +1,13 @@
+package cn.edu.sdwu.android.classroom.sn170507180224;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Ch1_Activity01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_ch1);
+    }
+}
